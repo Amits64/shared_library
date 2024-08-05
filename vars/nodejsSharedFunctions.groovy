578d01d242b1594de824b1d005ddad96713d1d80
Amits64 <chauhanamit090@hotmail.com>
2024-08-05 20:24:57 +0000

    updated jenkins shared pipeline

---
+++ vars/nodejsSharedFunctions.groovy
@@ -0,0 +1,50 @@
+def call(String registry, String registryCredential, String image, String tag, String sonarHostUrl) {
+    pipeline {
+        agent any
+
+        environment {
+            registry = registry
+            registryCredential = registryCredential
+            image = image
+            tag = tag
+        }
+
+        stages {
+            stage('Git Checkout') {
+                steps {
+                    checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: 'main']],
+                        userRemoteConfigs: [[url: 'https://github.com/Amits64/crud-app.git']]
+                    ])
+                }
+            }
+
+            stage('Static Code Analysis') {
+                steps {
+                    script {
+                        def sonarScannerImage = 'sonarsource/sonar-scanner-cli:latest'
+                        docker.image(sonarScannerImage).inside() {
+                            withSonarQubeEnv('sonarqube') {
+                                sh """
+                                sonar-scanner \
+                                -Dsonar.host.url=${sonarHostUrl} \
+                                -Dsonar.projectKey="${image}" \
+                                -Dsonar.exclusions=**/*.java
+                                """
+                            }
+                        }
+                    }
+                }
+            }
+
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        docker.build("${registry}/${image}:${tag}", "-f Dockerfile .")
+                    }
+                }
+            }
+        }
+    }
+}
