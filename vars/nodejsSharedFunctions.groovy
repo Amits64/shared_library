b965fca892137e02293e266511cb7b47f14b8574
Amits64 <chauhanamit090@hotmail.com>
2024-08-05 21:22:53 +0000

    updated shared pipeline

--- vars/nodejsSharedFunctions.groovy
+++ vars/nodejsSharedFunctions.groovy
@@ -1,48 +1,37 @@
-def call(String registry, String registryCredential, String image, String tag, String sonarHostUrl) {
-    pipeline {
-        agent any
-
-        environment {
-            registry = registry
-            registryCredential = registryCredential
-            image = image
-            tag = tag
+def ciPipeline(String registry, String registryCredential, String image, String tag, String sonarHostUrl) {
+    stages {
+        stage('Git Checkout') {
+            steps {
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: 'main']],
+                    userRemoteConfigs: [[url: 'https://github.com/Amits64/crud-app.git']]
+                ])
+            }
         }
 
-        stages {
-            stage('Git Checkout') {
-                steps {
-                    checkout([
-                        $class: 'GitSCM',
-                        branches: [[name: 'main']],
-                        userRemoteConfigs: [[url: 'https://github.com/Amits64/crud-app.git']]
-                    ])
-                }
-            }
-
-            stage('Static Code Analysis') {
-                steps {
-                    script {
-                        def sonarScannerImage = 'sonarsource/sonar-scanner-cli:latest'
-                        docker.image(sonarScannerImage).inside() {
-                            withSonarQubeEnv('sonarqube') {
-                                sh """
-                                sonar-scanner \
-                                -Dsonar.host.url=${sonarHostUrl} \
-                                -Dsonar.projectKey="${image}" \
-                                -Dsonar.exclusions=**/*.java
-                                """
-                            }
+        stage('Static Code Analysis') {
+            steps {
+                script {
+                    def sonarScannerImage = 'sonarsource/sonar-scanner-cli:latest'
+                    docker.image(sonarScannerImage).inside() {
+                        withSonarQubeEnv('sonarqube') {
+                            sh """
+                            sonar-scanner \
+                            -Dsonar.host.url=${sonarHostUrl} \
+                            -Dsonar.projectKey="${image}" \
+                            -Dsonar.exclusions=**/*.java
+                            """
                         }
                     }
                 }
             }
+        }
 
-            stage('Build Docker Image') {
-                steps {
-                    script {
-                        docker.build("${registry}/${image}:${tag}", "-f Dockerfile .")
-                    }
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    docker.build("${registry}/${image}:${tag}", "-f Dockerfile .")
                 }
             }
         }
